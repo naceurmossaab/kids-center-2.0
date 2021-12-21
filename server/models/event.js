8529ac30007484c602c44d14a779768d8a40e8c1
naceurmossaab <naceurmossaab@gmail.com>
2021-12-21 15:31:42 +0100

    services schemas update

--- server/models/event.js
+++ server/models/event.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 const eventSchema = mongoose.Schema(
   {
-    title:{ type: String },
-    eventBy: { type: Object },
+    title   :{ type: String },
+    eventBy : { type: Object },
     location: { type: String },
-    city: { type: String },
-    contact: { type: String, default: "" },
-    date: { type: String, default: "" },
-    event_img: {
+    city    : { type: String },
+    contact : { type: String, default: "" },
+    date    : { type: String, default: "" },
+    picture : {
       type: String,
       default:
         "https://www.sleeptasticsolutions.com/wp-content/uploads/2018/05/happy-kids-1.jpg",
--- server/models/services.js
+++ server/models/services.js
@@ -1,28 +1,19 @@
 var mongoose = require('mongoose');
 
-var serviceSchema = mongoose.Schema({
-  category: { type: String },
-  fullname: { type: String },
-  username: { type: String },
-  email   : { type: String },
-  specialty:{ type: String},
-  adresse : { type: String },
-  city    : { type: String },
-  phone   : { type: String, default : "" },
-  connect : { type: Boolean, default: false },
-  user_img: { type: String, default : "https://www.propertycentral.co.ke/assets/images/profiles/default.jpg" },
-},{ timestamps: true });
-
-
-//   "category":"serviceProvider",
-// "fullname":"test,
-// "specialty":"English",
-// "username":"jess",
-// "email":"jesser@gmail.com",
-// "adresse":"tunis",
-// "city":"tunis",
-// "phone":55
-
+const serviceSchema = mongoose.Schema(
+  {
+    title      : String,
+    description: String,
+    price      : String,
+    categorie  : String,
+    picture    : { type: String , default: "" },
+    available  : { type: Boolean, default: true },
+    user       : {
+      type: mongoose.Schema.Types.ObjectId,
+      ref : "User",
+                }
+  },{ timestamps: true, versionKey: false }
+);
 
 var Service = mongoose.model('Service', serviceSchema);
 
