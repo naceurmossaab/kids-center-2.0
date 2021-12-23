54521264a74196d195ea3ea08e461c0837dc821a
BelguesmiSakr <belguesmi_sakr@yahoo.com>
2021-12-23 22:06:50 +0100

    filter services by categorie and by adress

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,20 +1,28 @@
 <template>
-	<div class="services-conainer">
-		<div
-			class="service-conainer"
-			v-for="service in services"
-			:key="service._id"
-		>
-			<Service :service="service" />
+	<div class="main-box">
+		<Search
+			@filter-categorie="filterCategorie"
+			@filter-adress="filterAdress"
+		/>
+		<div class="services-conainer">
+			<div
+				class="service-conainer"
+				v-for="service in services"
+				:key="service._id"
+			>
+				<Service :service="service" />
+			</div>
 		</div>
 	</div>
 </template>
 <script>
 	const axios = require("axios");
 	import Service from "./Service.vue";
+	import Search from "./Search.vue";
+
 	export default {
 		name: "Services",
-		components: { Service },
+		components: { Service, Search },
 		data() {
 			return {
 				services: [],
@@ -25,6 +33,30 @@
 				.get("http://localhost:8000/user/service")
 				.then(({ data }) => (this.services = data));
 		},
+		methods: {
+			filterCategorie(categorie) {
+				console.log(categorie);
+				axios
+					.get("http://localhost:8000/user/service")
+					.then(
+						({ data }) =>
+							(this.services = data.filter(
+								(service) => service.service === categorie
+							))
+					);
+			},
+			filterAdress(adress) {
+				console.log(adress);
+				axios
+					.get("http://localhost:8000/user/service")
+					.then(
+						({ data }) =>
+							(this.services = data.filter(
+								(service) => service.location === adress
+							))
+					);
+			},
+		},
 	};
 </script>
 <style scoped>
@@ -34,10 +66,16 @@
 		grid-template-rows: 1fr;
 		grid-column-gap: 10px;
 		grid-row-gap: 20px;
-		background-size: cover;
+		/* background-size: cover;
 		background-image: url("https://images.unsplash.com/photo-1590467590164-c75b94a98575?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
 		background-attachment: fixed;
-		background-repeat: no-repeat;
+		background-repeat: no-repeat; */
 		padding: 0 200px;
+	}
+	.main-box {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
 	}
 </style>
